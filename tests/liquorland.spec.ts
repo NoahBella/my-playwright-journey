41c050412b0ccfc24edcf9791129c304f3a76047
Vicki Jeon <viickii25@gmail.com>
2026-04-03 21:22:13 +1300

    Updated

--- tests/liquorland.spec.ts
+++ tests/liquorland.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('My first automation test', async ({ page }) => {
+test('Age Gate & Newsletter Pop-Up', async ({ page }) => {
 
     //or const yesButton = page.getByRole ('button', {name: /yes/i});
     await page.goto('https://liquorland.co.nz/');
@@ -18,7 +18,15 @@ test('My first automation test', async ({ page }) => {
         await closePopUp.click();
     } else {
         console.log('Popup is not visible')
-    }
+    };
+
+    //await page.waitForTimeout(4000);
+    await page.waitForLoadState('networkidle');
+
+    await page.getByPlaceholder(/search/i).fill('Poco Vino');
+    await page.keyboard.press('Enter');
+
+    await page.waitForTimeout(4000);
 
 });
 
