34c6ca5c58e93b34ed0b846799c6b9fcf8f1af78
Vicki Jeon <viickii25@gmail.com>
2026-04-03 20:48:52 +1300

    Age Gate & Popup

--- tests/liquorland.spec.ts
+++ tests/liquorland.spec.ts
@@ -1,17 +1,24 @@
 import { test, expect } from '@playwright/test'
 
 test('My first automation test', async ({ page }) => {
-    const yesButton = page.getByRole('button', { name: 'Yes', exact: false });
 
+    //or const yesButton = page.getByRole ('button', {name: /yes/i});
     await page.goto('https://liquorland.co.nz/');
+    const yesButton = page.getByRole('button', { name: 'Yes', exact: false });
     // Robust approach: Wait for it to be visible, then click
     await yesButton.waitFor({ state: 'visible' });
+    //or await page.waitForTimeout(3000);
     await yesButton.click();
 
-    await page.getByLabel('Close Popup').click();
-    // await page.locator('.popup-overlay').hover(); <--- unable to find the locator so how to find it? 
+    //await page.getByLabel('Close Popup').click();
+    //or await page.locator('body').hover();
 
-
+    const closePopUp = page.getByLabel('Close');
+    if (await closePopUp.isVisible()) {
+        await closePopUp.click();
+    } else {
+        console.log('Popup is not visible')
+    }
 
 });
 
